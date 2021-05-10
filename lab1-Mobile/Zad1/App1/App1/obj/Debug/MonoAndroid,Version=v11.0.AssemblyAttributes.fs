7156c97e5f8e991caa01e5a2df04b6a053dd9285
gregorczykwiktor <48910956+gregorczykwiktor@users.noreply.github.com>
2021-05-10 16:26:53 +0200

    lab1-Mobile

---
+++ lab1-Mobile/Zad1/App1/App1/obj/Debug/MonoAndroid,Version=v11.0.AssemblyAttributes.fs
@@ -0,0 +1,3 @@
+namespace Microsoft.BuildSettings
+                [<System.Runtime.Versioning.TargetFrameworkAttribute("MonoAndroid,Version=v11.0", FrameworkDisplayName="Xamarin.Android v11.0 Support")>]
+                do ()
---
+++ lab1-Mobile/Zad2/App3/App3/App3/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Lab3;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
